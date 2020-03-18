7df5c11bb4742eebd907ed60fce74fb8a2d2e7d7
popkovvvv <popkovvvv@mail.ru>
2020-03-18 19:54:03 +0300

    quick fixes

--- server/src/main/java/com/smart/design/config/WebSocketConfig.java
+++ server/src/main/java/com/smart/design/config/WebSocketConfig.java
@@ -17,12 +17,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
 
-
-        registry.enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
-                .setRelayPort(61613)
-                .setClientLogin("guest")
-                .setClientPasscode("guest");
-
     }
 }
